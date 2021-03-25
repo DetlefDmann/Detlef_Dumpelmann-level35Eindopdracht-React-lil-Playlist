ac3a8d53019f9b40e28e80f58fb4ff6318e9b718
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-03-25 17:07:01 +0100

    FilterSlice redux definitions

--- playlist/src/app/store.js
+++ playlist/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import songsReducer from '../features/songs/songSlice';
+import filterReducer from '../features/filters/filterSlice';
 
 export default configureStore({
   reducer: {
     songs: songsReducer,
+    filters: filterReducer,
   },
 });
