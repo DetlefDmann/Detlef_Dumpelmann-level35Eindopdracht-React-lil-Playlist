b85c5f741ea017e475b2895fa9241187be35910c
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-03-25 21:01:22 +0100

    Reordered components to right folders

--- playlist/src/components/SongForm.js
+++ playlist/src/components/SongForm.js
@@ -1,6 +1,6 @@
 import React, {useState } from 'react'
 import {useDispatch} from 'react-redux';
-import { addSong } from './songSlice';
+import { addSong } from '../features/songs/songSlice';
 
 
 const SongForm = () => {
