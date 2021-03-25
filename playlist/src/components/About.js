8c034e1de51bb72206bc81d51d52f5777430736f
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-03-25 16:33:28 +0100

    add redux to about page

--- playlist/src/components/About.js
+++ playlist/src/components/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectSongs } from '.././features/songs/songSlice';
+
 
 const About = () => {
+    const songs  = useSelector(selectSongs);
     return (
         <main>
-            About me
+            <h3>About this songlist:</h3> 
+            <p>There are currently {songs.length} songs on this list.</p>
         </main>
     )
 }
