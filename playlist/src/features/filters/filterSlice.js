de7a349a0888403cdcbe1c12e891f03f400b2d2b
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-03-27 15:49:17 +0100

    genreselection fixed

--- playlist/src/features/filters/filterSlice.js
+++ playlist/src/features/filters/filterSlice.js
@@ -3,14 +3,35 @@ import { createSlice } from '@reduxjs/toolkit';
 export const filterSlice = createSlice({
     name:'filters',
     initialState: {
-        genre:"all",
-        stars:0,
+        genres:{
+            metalSongArray:true,
+            rockSongArray:true,
+            bluesSongArray:true,
+            klassiekSongArray:true,
+            jazzSongArray:true,
+            popSongArray:true,
+            fusionSongArray:true,
+            soulSongArray:true,
+            reggaeSongArray:true,
+            skaSongArray:true,
+            punkSongArray:true,
+            grungeSongArray:true,
+            elevatorSongArray:true,
+        },
+        stars:{
+            0:true,
+            1:true,
+            2:true,
+            3:true,
+            4:true,
+            5:true,
+        },
     },
     reducers:{
-        filterGenre: (state , genre) => {
-            return {...state,
-                genre
-            }
+        filterGenre: (state , genrestate) => {
+            
+                state.genres[genrestate.payload.name]= genrestate.payload.checked
+            
         },
         filterStars: (state , stars) => {
             return {...state,
@@ -22,7 +43,7 @@ export const filterSlice = createSlice({
 
 export const { filterGenre , filterStars } = filterSlice.actions;
 
-export const selectGenre = state => state.filters.genre;
+export const selectGenre = state => state.filters.genres;
 
 export const selectStars = state => state.filters.stars;
 
