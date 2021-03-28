889078c4f0a4674c72d447e9dd8d0905684d1d94
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-03-28 21:29:23 +0200

    SongSortingPanel initial reference

---
+++ playlist/src/components/SongSortingPanel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SongSortingPanel = () => {
+    return (
+        <div className="song__sort">
+            <p>Sort songs by</p>
+        </div>
+    )
+}
+
+export default SongSortingPanel
