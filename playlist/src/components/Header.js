8cde06ccb21923409443abe6da2fc736bac17978
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-04-01 12:50:10 +0200

    BEM naming

--- playlist/src/components/Header.js
+++ playlist/src/components/Header.js
@@ -4,7 +4,7 @@ import logo from '.././logo.svg';
 const Header = () => {
     return (
         <header className="App__header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <img src={logo} className="App__logo" alt="logo" />
             <h1>My Favorite Songs</h1>
       </header>
     )
