312cd90d17d7e9649488e92a43dec43b040a42e9
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-10-11 21:51:47 +0200

    added some tests for app.js

--- playlist/src/App.js
+++ playlist/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
-import { SongOverview } from './components/SongOverview' ;
-import About from './components/About';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { SongOverview } from "./components/SongOverview";
+import About from "./components/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Router >
+      <Router>
         <Header />
         <NavBar />
-          <Switch>
-            <Route path="/about" >
-              <About />
-            </Route>
-            <Route path="/" >
-              <SongOverview />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <SongOverview />
+          </Route>
+        </Switch>
         <Footer />
       </Router>
     </div>
--- playlist/src/App.test.js
+++ playlist/src/App.test.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { describe } from "@jest/globals";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
+describe("Test if items are being rendered", () => {
+  test("renders element with Text: My Favorite Songs", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(getByText(/My Favorite Songs/i)).toBeInTheDocument();
+  });
+  test("renders element with Text: Sort songs by", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(getByText(/Sort songs by/i)).toBeInTheDocument();
+  });
+  test("renders element with Text: Detlef Dumpelmann", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(getByText(/Detlef Dumpelmann/i)).toBeInTheDocument();
+  });
+  test("[about Page] renders element with Text: About this songlist:", () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <App path="/about" />
+      </Provider>
+    );
+    const aboutLink = document.querySelector("#about");
+    aboutLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    expect(getByText(/About this songlist:/i)).toBeInTheDocument();
+  });
 });
