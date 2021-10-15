8bb271a529979c56e6a0aa1a1d0fdb4139ffb0a6
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-10-15 18:43:24 +0200

    better statics?

--- playlist/public/service-worker.js
+++ playlist/public/service-worker.js
@@ -2,7 +2,14 @@
 
 const VERSION_NR_STATIC = "static-v1";
 const VERSION_NR_DYNAMIC = "dynamic-v1";
-const STATIC_FILES = ["./", "./about", "./home", "./index.html", "/src/App.js"];
+const STATIC_FILES = [
+  "./",
+  "./about",
+  "./home",
+  "./index.html",
+  "./static/App.js",
+  "./static/index.js",
+];
 
 //add minimal necessary files to cache
 self.addEventListener("install", (event) => {
